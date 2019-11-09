49e42e7e10e1e7df4a1fceaad8212c1c220dcaa8
Travis Clark <travis6clark@gmail.com>
2019-11-09 13:42:29 -0800

    initial backend build

--- client/App.jsx
+++ client/App.jsx
@@ -1,4 +1,6 @@
 import React from  "react";
+import axios from 'axios';
+
 import MainContainer from "./containers/MainContainer.jsx";
 
 //We used functional components throughout the application so we could utilize hooks
@@ -9,9 +11,26 @@ import MainContainer from "./containers/MainContainer.jsx";
 //We didn't instantiate state at the parent level because none of the state we use relates to other components
 
 
-const App = () => {
+//straightforward axios GET request to route '/news'
+    //I initially factored this into a constructor, then set state with the result
+    //implement at will, Frontend
+
+function getNews(){
+    axios.get(`http://localhost:3000/news`)
+    .then(res => {
+        //response will come as a nested array - each nested array is populated with articles from a unique source
+            //eg. LA Times, Youtube, etc.
+            //[
+            //     [ { title: 'LA Times Title', link: 'LATimes.com' ],
+            //     [ { title: 'Youtube Title', link: 'youtube.com' ]
+            // ]
+        console.log(res)
+    })
+}
+
+const App = () => {  
     return (
-        <MainContainer/>
+        <MainContainer />
       );
 }
  
