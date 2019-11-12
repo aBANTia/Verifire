21d43d8320492d65f8356d53394dd28214b95363
bmwoolf <bradleymwoolf@gmail.com>
2019-11-11 20:38:52 -0800

    added more commentary

--- server/models/models.js
+++ server/models/models.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const MONGO_URI = "mongodb+srv://bradleyDB:kiroismypartner@cluster0-klwdv.mongodb.net/test?retryWrites=true&w=majority";
 
+//connect to database
 mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -10,6 +11,7 @@ mongoose.connect(MONGO_URI, {
 .then(() => console.log('Connected to Mongo DB.'))
 .catch(err => console.log(err));
 
+//create new schema for the messages
 const messageSchema = new Schema({
     name: String,
     message: String
